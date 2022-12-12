81913387680f87d973f3349501b94362d23b8099
Leal <lealnetosena@gmail.com>
2022-12-12 19:47:29 -0300

    Intro a middleware

--- 4.ProgramacaoWeb2/ExpressApiTypescript/src/server.ts
+++ 4.ProgramacaoWeb2/ExpressApiTypescript/src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 
+import logs from './middlewares/logs';
 import routes from './routes';
 
 const port = 3333;
 const server = express()
 
+// Middleware
 // Habilitando a deserialização de JSON
 server.use(express.json());
+
+server.use(logs);
+
 
 // Adicionar as rotas 
 server.use(routes)
